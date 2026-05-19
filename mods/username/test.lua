4cad02f28de6be8e07e4ea7d97f3f3b5c45c2dfa
Alpaca Communist <alpaca.communist@gmail.com>
2026-05-19 20:04:25 +0300

    adds lua frontend for game start event, work in progress

--- mods/username/test.lua
+++ mods/username/test.lua
@@ -1,4 +1,4 @@
--- mods/userspace/test.lua
+-- mods/username/test.lua
 
 -- checks if lua even works
 print("hello from lua to linux terminal (still works!)")
@@ -6,5 +6,14 @@ print("hello from lua to linux terminal (still works!)")
 
 -- checks ck api
 local log = require('fallout2.log')
-
 log.print("HELLO FROM MOD VIA REQUIRE!!!")
+
+-- this goes into fallout2.events in the future
+function ckOnGameStart()
+    print("[Lua] Game is Ready! Triggering onGameStart hook...")
+    
+    -- Try printing text into interface monitor!
+    log.print("STATION CK: ONLINE AND READY TO MOD!")
+    log.print("You see: A Giant Ant! Oh wait, actually it's LuaJit!")
+end
+
