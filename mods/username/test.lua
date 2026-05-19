eef1211b148821cfc3d511c511e39536a0b954b3
Alpaca Communist <alpaca.communist@gmail.com>
2026-05-19 15:52:59 +0300

    prepares lua frontend, work in progress

--- mods/username/test.lua
+++ mods/username/test.lua
@@ -1,1 +1,10 @@
-print("<<< Hello from Lua! >>>")
+-- mods/userspace/test.lua
+
+-- checks if lua even works
+print("hello from lua to linux terminal (still works!)")
+
+
+-- checks ck api
+local log = require('fallout2.log')
+
+log.print("HELLO FROM MOD VIA REQUIRE!!!")
