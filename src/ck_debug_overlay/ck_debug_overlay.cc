bc7bb46f48725bcb4e078854dca0f1fb356017cd
Alpaca Communist <alpaca.communist@gmail.com>
2026-06-01 10:43:21 +0300

    adds mode_main to ck_debug_overlay, work in progress

--- src/ck_debug_overlay/ck_debug_overlay.cc
+++ src/ck_debug_overlay/ck_debug_overlay.cc
@@ -2,13 +2,60 @@
 #include "ck_debug_overlay/ck_debug_overlay_render.h"
 #include "ck_debug_overlay/ck_debug_overlay_colors.h"
 
-#include "mouse.h"
+#include <unordered_set>
+
 #include "display_monitor.h"
+#include "mouse.h"
 #include "debug.h"
 #include "tile.h"
+#include "object.h"
 
 
 static bool gDebugOverlayEnabled = false;
+
+static std::unordered_set<int> gScannedTiles;
+
+static void mode_main(int anchorTile) {
+	static int sLastDudeTile = -1;
+
+	fallout::Object* dude = fallout::gDude;
+
+    if (dude == nullptr) return;
+	if (dude->tile == sLastDudeTile) return;
+
+	sLastDudeTile = dude->tile;
+
+    int anchorScreenX, anchorScreenY;
+    fallout::tileToScreenXY(anchorTile, &anchorScreenX, &anchorScreenY);
+
+    const int radius = 3;
+
+    for (int tile = dude->tile - radius;
+         tile <= dude->tile + radius;
+         tile++) {
+
+        if (!fallout::hexGridTileIsValid(tile)) continue;
+
+		// already scanned
+        if (gScannedTiles.find(tile) != gScannedTiles.end()) continue;
+
+        gScannedTiles.insert(tile);
+
+        int screenX, screenY;
+        fallout::tileToScreenXY(tile, &screenX, &screenY);
+
+        fallout::Object* blocker = fallout::_obj_blocking_at( nullptr, tile, fallout::gElevation);
+
+        if (blocker != nullptr && blocker->fid != dude->fid) {
+            fallout::debugPrint("[CK] BLOCKER tile=%d pid=%d fid=%d flags=%08X\n",
+                tile, blocker->pid, blocker->fid, blocker->flags);
+
+            ck_debug_overlay_add_hex(996, anchorTile, screenX - anchorScreenX, screenY - anchorScreenY, RED);
+        } else {
+            ck_debug_overlay_add_hex(997, anchorTile, screenX - anchorScreenX, screenY - anchorScreenY, BLUE);
+        }
+    }
+}
 
 bool ck_debug_overlay_enabled() {
     return gDebugOverlayEnabled;
@@ -21,7 +68,9 @@ void ck_debug_overlay_toggle() {
 		fallout::displayMonitorAddMessage("[CK] Debug Overlay: ON");
 	} else {
 		fallout::displayMonitorAddMessage("[CK] Debug Overlay: OFF");
+
 		ck_debug_overlay_clear();
+		gScannedTiles.clear();
 		ck_debug_overlay_shutdown();
 	}
 }
@@ -65,9 +114,11 @@ void ck_debug_overlay_render(fallout::Rect* rect) {
     if (!gDebugOverlayEnabled) return;
 
 	ck_debug_overlay_persistent_hexes(rect);
+
+	mode_main(17290);
 	if ((fallout::mouseGetEvent() & MOUSE_EVENT_LEFT_BUTTON_REPEAT) != 0) {
 		// working_hover(17290);
-		palette_paint_hover(17290);
+		// palette_paint_hover(17290);
 	}
 }
 
