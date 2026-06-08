c528e5d96b6d274458f072509a42332b8382aa1b
Alpaca Communist <alpaca.communist@gmail.com>
2026-06-09 01:33:26 +0300

    merges engine upstream

--- src/ck_scripting.cc
+++ src/ck_scripting.cc
@@ -205,7 +205,7 @@ void ck_scripting_on_game_start() {
 
 void ck_scripting_on_engine_ready() {
     std::cout << "[CK] Engine ready, initializing proto cache..." << std::endl;
-    gProtoCache.initialize("build/proto_cache.json");
+    // gProtoCache.initialize("build/proto_cache.json");
 }
 
 void ck_scripting_on_game_loaded() {
