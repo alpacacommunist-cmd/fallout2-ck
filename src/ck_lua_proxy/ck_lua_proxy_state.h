b9706fb0fc7e2271d5535547acb107ad4c84806f
Alpaca Communist <alpaca.communist@gmail.com>
2026-06-30 15:39:01 +0300

    exit frags

--- src/ck_lua_proxy/ck_lua_proxy_state.h
+++ src/ck_lua_proxy/ck_lua_proxy_state.h
@@ -2,8 +2,9 @@
 #define CK_LUA_PROXY_STATE_H
 
 #include "ck_lua_proxy/ck_lua_proxy.h"
-#include "picojson.h"
 #include <string>
+
+namespace picojson { class value; }
 
 namespace ck::proxy {
     int get_state_tile(const int map_id, const std::string& lua_tag);
