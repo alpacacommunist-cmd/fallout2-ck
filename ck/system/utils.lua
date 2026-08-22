8960ca94b2adce23fabb54d74f16964c8884c2c0
Alpaca Communist <alpaca.communist@gmail.com>
2026-08-22 22:19:28 +0300

    lua utils helpers

--- ck/system/utils.lua
+++ ck/system/utils.lua
@@ -13,4 +13,31 @@ function utils.print_table(t, log, indent)
   end
 end
 
+function utils.shallow_copy(orig)
+  if type(orig) ~= 'table' then return orig end
+
+  local copy = {}
+  for k, v in pairs(orig) do
+    copy[k] = v
+  end
+  return copy
+end
+
+function utils.deep_copy(orig)
+  local orig_type = type(orig)
+  local copy
+
+  if orig_type == 'table' then
+    copy = {}
+    for orig_key, orig_value in next, orig, nil do
+      copy[utils.deep_copy(orig_key)] = utils.deep_copy(orig_value)
+    end
+    setmetatable(copy, utils.deep_copy(getmetatable(orig)))
+  else
+    copy = orig
+  end
+
+  return copy
+end
+
 return utils
