b74579bad7e10ee6a16d521d065527af6ee0e260
Alpaca Communist <alpaca.communist@gmail.com>
2026-09-07 22:57:19 +0300

    lua log headers aura chck

--- ck/system/log.lua
+++ ck/system/log.lua
@@ -9,9 +9,17 @@ local ANSI = {
   RED    = "\27[31m",
   GREEN  = "\27[32m",
   YELLOW = "\27[33m",
+  BLUE    = "\27[34m",
+  MAGENTA = "\27[35m",
   CYAN   = "\27[36m",
   WHITE  = "\27[37m",
   GRAY   = "\27[90m"
+}
+
+log_module.Style = {
+  Minimal   = "minimal",
+  Cyberpunk = "cyberpunk",
+  Modern    = "modern"
 }
 
 local function colorize(color_code, text)
@@ -21,10 +29,57 @@ local function colorize(color_code, text)
   return text
 end
 
+local function utf8_len(str)
+  if utf8 and utf8.len then
+    return utf8.len(str) or string.len(str)
+  end
+  return string.len(str)
+end
+
 function log_module.new(prefix)
   local logger = {}
 
   local formatted_prefix = colorize(ANSI.BOLD .. ANSI.WHITE, "[" .. prefix .. "]")
+  local clean_prefix_len = utf8_len(prefix) + 2
+
+  function logger.header(style, message, ...)
+    if type(style) ~= "string" or (style ~= "minimal" and style ~= "cyberpunk" and style ~= "modern") then
+      return logger.header(log_module.Style.Minimal, style, message, ...)
+    end
+
+    local formatted_title = (select('#', ...) > 0) and string.format(message, ...) or message
+    local total_width = 80
+
+    if style == log_module.Style.Minimal then
+      local used_len = 4 + clean_prefix_len + 5 + utf8_len(formatted_title) + 1
+      local fill_chars = (total_width > used_len) and (total_width - used_len) or 0
+
+      local line_decor = colorize(ANSI.BLUE, "───")
+      local mid_decor  = colorize(ANSI.BLUE, " ─── ")
+      local fill_line  = colorize(ANSI.BLUE, string.rep("─", fill_chars))
+      local title_part = colorize(ANSI.BOLD .. ANSI.WHITE, formatted_title)
+
+      print(string.format("%s %s%s %s %s", line_decor, formatted_prefix, mid_decor, title_part, fill_line))
+
+    elseif style == log_module.Style.Cyberpunk then
+      local used_len = 4 + clean_prefix_len + 5 + utf8_len(formatted_title) + 1
+      local fill_chars = (total_width > used_len) and (total_width - used_len) or 0
+
+      local block_decor = colorize(ANSI.BRIGHT_GREEN, "▒▒▒")
+      local mid_decor   = colorize(ANSI.BRIGHT_GREEN, " ▒▒▒ ")
+      local fill_line   = colorize(ANSI.BRIGHT_GREEN, string.rep("▒", fill_chars))
+      local title_part  = colorize(ANSI.BOLD .. ANSI.BRIGHT_GREEN, formatted_title)
+
+      print(string.format("%s %s%s %s %s", block_decor, formatted_prefix, mid_decor, title_part, fill_line))
+
+    elseif style == log_module.Style.Modern then
+      local arrow1 = colorize(ANSI.YELLOW, "❯❯❯")
+      local arrow2 = colorize(ANSI.YELLOW, "❯")
+      local title_part = colorize(ANSI.BOLD, formatted_title)
+
+      print(string.format("%s %s %s %s", arrow1, formatted_prefix, arrow2, title_part))
+    end
+  end
 
   function logger.info(message, ...)
     local formatted = (select('#', ...) > 0) and string.format(message, ...) or message
@@ -52,44 +107,6 @@ function log_module.new(prefix)
     local tag = colorize(ANSI.BOLD .. ANSI.RED, "[ERROR]")
     local text = colorize(ANSI.RED, tostring(formatted))
     print(string.format("%s %s %s", formatted_prefix, tag, text))
-  end
-
-  function logger.header(message, ...)
-    local formatted = (select('#', ...) > 0) and string.format(message, ...) or message
-    local msg_str = tostring(formatted)
-
-    -- "=" line to match text length
-    local border = string.rep("=", string.len(msg_str))
-    local colored_border = colorize(ANSI.CYAN, border)
-
-    print(string.format("%s %s\n%s %s\n%s %s",
-      formatted_prefix, colored_border,
-      formatted_prefix, msg_str,
-      formatted_prefix, colored_border))
-  end
-
-  function logger.fixed_header(message, ...)
-    local formatted = (select('#', ...) > 0) and string.format(message, ...) or message
-    local total_width = 40
-
-    local msg_str = tostring(formatted)
-    local msg_length = string.len(msg_str)
-
-    local border = string.rep("=", total_width)
-    local colored_border = colorize(ANSI.CYAN, border)
-
-    -- centers text in fixed width
-    local spaces_needed = total_width - msg_length
-    if spaces_needed > 0 then
-      local left_spaces = math.floor(spaces_needed / 2)
-      local right_spaces = spaces_needed - left_spaces
-      msg_str = string.rep(" ", left_spaces) .. msg_str .. string.rep(" ", right_spaces)
-    end
-
-    print(string.format("%s %s\n%s %s\n%s %s",
-      formatted_prefix, colored_border,
-      formatted_prefix, msg_str,
-      formatted_prefix, colored_border))
   end
 
   return logger
